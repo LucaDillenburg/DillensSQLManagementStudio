cdd194cc54aa6128be275fbce7f58ac3c93add5e
LucaDillenburg <LucaDillenburg@users.noreply.github.com>
2018-06-13 22:50:53 -0300

    Primeiro Commit
    Falta programar btnSave, btnOtherDtBs e melhorar btnExecute se rdAutomatic.Checked (ver "O que falta")

---
+++ DillenManagementStudio/DillenManagementStudio/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DillenManagementStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grvSelect = new System.Windows.Forms.DataGridView();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.RichTextBox();
+            this.rdSelect = new System.Windows.Forms.RadioButton();
+            this.rdNonQuery = new System.Windows.Forms.RadioButton();
+            this.btnChangeDtBs = new System.Windows.Forms.Button();
+            this.cbxChsDtBs = new System.Windows.Forms.ComboBox();
+            this.rdAutomatic = new System.Windows.Forms.RadioButton();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOtherDtBs = new System.Windows.Forms.Button();
+            this.lbTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grvSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grvSelect
+            // 
+            this.grvSelect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvSelect.Location = new System.Drawing.Point(780, 86);
+            this.grvSelect.Name = "grvSelect";
+            this.grvSelect.ReadOnly = true;
+            this.grvSelect.Size = new System.Drawing.Size(578, 609);
+            this.grvSelect.TabIndex = 20;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.Location = new System.Drawing.Point(294, 635);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(161, 48);
+            this.btnExecute.TabIndex = 19;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(12, 140);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(749, 465);
+            this.txtCode.TabIndex = 18;
+            this.txtCode.Text = "Write down your SQL code in here...";
+            // 
+            // rdSelect
+            // 
+            this.rdSelect.AutoSize = true;
+            this.rdSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdSelect.Location = new System.Drawing.Point(91, 642);
+            this.rdSelect.Name = "rdSelect";
+            this.rdSelect.Size = new System.Drawing.Size(78, 24);
+            this.rdSelect.TabIndex = 17;
+            this.rdSelect.Text = "Select";
+            this.rdSelect.UseVisualStyleBackColor = true;
+            // 
+            // rdNonQuery
+            // 
+            this.rdNonQuery.AutoSize = true;
+            this.rdNonQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNonQuery.Location = new System.Drawing.Point(91, 621);
+            this.rdNonQuery.Name = "rdNonQuery";
+            this.rdNonQuery.Size = new System.Drawing.Size(106, 24);
+            this.rdNonQuery.TabIndex = 16;
+            this.rdNonQuery.Text = "NonQuery";
+            this.rdNonQuery.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeDtBs
+            // 
+            this.btnChangeDtBs.Location = new System.Drawing.Point(476, 93);
+            this.btnChangeDtBs.Name = "btnChangeDtBs";
+            this.btnChangeDtBs.Size = new System.Drawing.Size(113, 23);
+            this.btnChangeDtBs.TabIndex = 15;
+            this.btnChangeDtBs.Text = "Change database";
+            this.btnChangeDtBs.UseVisualStyleBackColor = true;
+            this.btnChangeDtBs.Click += new System.EventHandler(this.btnChangeDtBs_Click);
+            // 
+            // cbxChsDtBs
+            // 
+            this.cbxChsDtBs.FormattingEnabled = true;
+            this.cbxChsDtBs.Items.AddRange(new object[] {
+            "My - BD17188",
+            "Prática Prof. - BDPRII17188"});
+            this.cbxChsDtBs.Location = new System.Drawing.Point(12, 93);
+            this.cbxChsDtBs.Name = "cbxChsDtBs";
+            this.cbxChsDtBs.Size = new System.Drawing.Size(443, 21);
+            this.cbxChsDtBs.TabIndex = 14;
+            // 
+            // rdAutomatic
+            // 
+            this.rdAutomatic.AutoSize = true;
+            this.rdAutomatic.Checked = true;
+            this.rdAutomatic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdAutomatic.Location = new System.Drawing.Point(91, 663);
+            this.rdAutomatic.Name = "rdAutomatic";
+            this.rdAutomatic.Size = new System.Drawing.Size(108, 24);
+            this.rdAutomatic.TabIndex = 21;
+            this.rdAutomatic.TabStop = true;
+            this.rdAutomatic.Text = "Automatic";
+            this.rdAutomatic.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(565, 653);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(86, 30);
+            this.btnSave.TabIndex = 22;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnOtherDtBs
+            // 
+            this.btnOtherDtBs.Location = new System.Drawing.Point(669, 93);
+            this.btnOtherDtBs.Name = "btnOtherDtBs";
+            this.btnOtherDtBs.Size = new System.Drawing.Size(75, 23);
+            this.btnOtherDtBs.TabIndex = 23;
+            this.btnOtherDtBs.Text = "Other";
+            this.btnOtherDtBs.UseVisualStyleBackColor = true;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Modern No. 20", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(338, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(703, 50);
+            this.lbTitle.TabIndex = 24;
+            this.lbTitle.Text = "Dillen\'s SQL Management Studio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 703);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.btnOtherDtBs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.rdAutomatic);
+            this.Controls.Add(this.grvSelect);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.rdSelect);
+            this.Controls.Add(this.rdNonQuery);
+            this.Controls.Add(this.btnChangeDtBs);
+            this.Controls.Add(this.cbxChsDtBs);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dillenburg\'s Product";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grvSelect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grvSelect;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.RichTextBox txtCode;
+        private System.Windows.Forms.RadioButton rdSelect;
+        private System.Windows.Forms.RadioButton rdNonQuery;
+        private System.Windows.Forms.Button btnChangeDtBs;
+        private System.Windows.Forms.ComboBox cbxChsDtBs;
+        private System.Windows.Forms.RadioButton rdAutomatic;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOtherDtBs;
+        private System.Windows.Forms.Label lbTitle;
+    }
+}
+
---
+++ DillenManagementStudio/DillenManagementStudio/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//para banco
+using System.Data.SqlClient;
+
+namespace DillenManagementStudio
+{
+    public partial class Form1 : Form
+    {
+        protected string connStr;
+        protected SqlConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cbxChsDtBs.SelectedIndex = 0;
+
+            btnChangeDtBs.PerformClick();
+        }
+
+        private void btnChangeDtBs_Click(object sender, EventArgs e)
+        {
+            //stablish conection with the datebase
+            switch (cbxChsDtBs.SelectedIndex)
+            {
+                case 0:
+                    this.connStr = Properties.Settings.Default.BD17188ConnectionString;
+                    break;
+                case 1:
+                    this.connStr = Properties.Settings.Default.BDPRII17188ConnectionString;
+                    break;
+                default:
+                    throw new Exception("btnEscBanco_Click error due to new datebase!");
+            }
+
+            try
+            {
+                this.con = new SqlConnection();
+                this.connStr = this.connStr.Substring(this.connStr.IndexOf("Data Source"));
+                this.con.ConnectionString = this.connStr;
+                this.con.Open();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("An error occurred when trying to connect to the database chosen! \n\rSee if you are with the Unicamp VPN and connected to the internet! \n\rRESTART THE PROGRAM!");
+                this.Close();
+            }
+
+            MessageBox.Show("Database connected!");
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            //put txtCode.Items in a String (with spaces between each line)
+            String code = "";
+            for (int i = 0; i < this.txtCode.Lines.Length; i++)
+                code += " " + this.txtCode.Lines[i];
+
+            //execute query or nonQuery depending on the radioButtons
+            //if it's a query, show it in the DataGridView 
+            //else try to show select of that whole table that was changed
+
+            bool queryExecution;
+            if(this.rdAutomatic.Checked)
+            {
+                int indexSelect = code.IndexOf("select", StringComparison.CurrentCultureIgnoreCase);
+                queryExecution = (indexSelect >= 0) && (code.IndexOf("'", 0, indexSelect) < 0);
+
+                if(!queryExecution)
+                {
+                    indexSelect = code.IndexOf("sp_help", StringComparison.CurrentCultureIgnoreCase);
+                    queryExecution = (indexSelect >= 0) && (code.IndexOf("'", 0, indexSelect) < 0);
+                }
+            }else
+            {
+                queryExecution = this.rdSelect.Checked;
+            }
+            
+
+            if (!queryExecution && !String.IsNullOrWhiteSpace(code))
+            {
+                try
+                {
+                    SqlCommand cmdNQ = new SqlCommand(code, this.con);
+
+                    int iResult = cmdNQ.ExecuteNonQuery();
+
+                    //Say if the non Query execution was completed
+                    if (iResult <= 0)
+                    {
+                        MessageBox.Show("Non Query error! No exception raised...");
+                        return;
+                    }
+                    else
+                        MessageBox.Show("Non Query completed!");
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("Non Query error: " + err);
+                    return;
+                }
+
+                //transform code in the code to see all the top 50 of a table
+                int lastIndexOfInsert = code.LastIndexOf("Insert", StringComparison.CurrentCultureIgnoreCase); //not case sensitive
+                int lastIndexOfUpdate = code.LastIndexOf("Update", StringComparison.CurrentCultureIgnoreCase); //not case sensitive
+
+                //Select from UPDATED table or INSERTED table
+                int lastIndexOfAll;
+
+                if (lastIndexOfInsert > lastIndexOfUpdate)
+                    lastIndexOfAll = code.IndexOf("into", lastIndexOfInsert, StringComparison.CurrentCultureIgnoreCase) + 4;
+                else
+                    lastIndexOfAll = lastIndexOfUpdate + 6;
+
+                if (lastIndexOfAll < 0)
+                    return;
+
+                int whereFirstLetter = -1;
+                for (int i = lastIndexOfAll; ; i++)
+                    if (code[i] != ' ')
+                    {
+                        whereFirstLetter = i;
+                        break;
+                    }
+
+                int length = code.IndexOf(" ", whereFirstLetter) - whereFirstLetter;
+                String tableName = code.Substring(whereFirstLetter, length);
+                code = "Select top(100)* from " + tableName;
+            }
+
+            if (!String.IsNullOrWhiteSpace(code))
+            {
+                DataSet ds = new DataSet();
+
+                try
+                {
+                    SqlCommand cmdQ = new SqlCommand(code, this.con);
+
+                    SqlDataAdapter adapt = new SqlDataAdapter(cmdQ);
+
+                    adapt.Fill(ds);
+
+                    if(ds.Tables.Count < 0)
+                    {
+                        DataTable tab = new DataTable();
+                        this.grvSelect.DataSource = tab;
+                        MessageBox.Show("Non Query error! No exception raised...");
+                        return;
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("Non Query error: " + err);
+                    return;
+                }
+
+                //change number of rows and columns of the DataGridView
+                //put the names of the columns
+                DataTable table = new DataTable();
+
+                for(int i = 0; i<ds.Tables[0].Columns.Count; i++)
+                    table.Columns.Add(ds.Tables[0].Columns[i].ColumnName + " (" + ds.Tables[0].Columns[i].DataType.ToString() + ")", typeof(string));
+
+                for (int ir = 0; ir < ds.Tables[0].Rows.Count; ir++)
+                    table.Rows.Add(ds.Tables[0].Rows[ir].ItemArray);
+
+                this.grvSelect.DataSource = table;
+
+                //If the person asked to execute a Query, say if the Query execution was completed
+                if (rdSelect.Checked)
+                    MessageBox.Show("Selection completed!");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.con.Close();
+        }
+    }
+}
